efca6d3e298a867edc2da91899f2de95978625eb
EC2 Default User <ec2-user@ip-172-31-15-240.ap-northeast-1.compute.internal>
2023-02-05 06:00:51 +0000

    Add sign in, up, out and add homes top and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
