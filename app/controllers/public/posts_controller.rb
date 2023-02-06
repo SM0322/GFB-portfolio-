e01a551935c328f41e1c10e93376a08a21b50a8a
EC2 Default User <ec2-user@ip-172-31-15-240.ap-northeast-1.compute.internal>
2023-02-06 02:28:46 +0000

    Add several tags post

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,11 +6,15 @@ class Public::PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.customer_id = current_customer.id
+    tag_list=params[:post][:name].split(',')         #splitは()の中の引数により文字列を分割し、分割された各文字列を要素としている。今回は「'」で区切られた文字列を要素としている。
     @post.save
+    @post.save_tag(tag_list)                         #modelファイルにて定義されたメソッド
     redirect_to public_posts_index_path
   end 
 
   def index
+    @posts = Post.all
+    @tags = Tag.all
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,20 @@
 class Post < ApplicationRecord
   belongs_to :customer
+  has_many :post_tags, dependent: :destroy
+  has_many :tags,through: :post_tags
+  
+  def save_tag(sent_tags)
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?   #unlessでタグが存在するか確認し、存在するならばcurrent_tagsという変数にタグ名を配列として習得する。
+    old_tags = current_tags - sent_tags                           #(例)既にa,b,cのタグが存在し、新たにb,dのタグが登録されると、old_tags = a,cとなる
+    new_tags = sent_tags - current_tags                           #(例)既にa,b,cのタグが存在し、新たにb,dのタグが登録されると、new_tags = dとなる
+    
+    old_tags.each do |old|                                        #このメソッドで古いタグを消去する。例でいうところのold_tags = a,cが消去される。
+      self.tags.delete　tag.find_by(name: old)
+    end 
+    
+    new_tags.each do |new|                                        #このメソッドで新しく登録されたnew_tags = dが保存される。
+      new_post_tag = Tag.find_or_create_by(name: new)
+      self.tags << new_post_tag
+    end 
+  end 
 end
