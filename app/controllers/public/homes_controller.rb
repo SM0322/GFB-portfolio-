07ffac5bfc42673703a352c0b7a3ac43b1d880b4
EC2 Default User <ec2-user@ip-172-31-15-240.ap-northeast-1.compute.internal>
2023-02-14 02:19:58 +0000

    Change home about sign_in sign_up layouts

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    @post_favorite_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+    @post_news = Post.order('id DESC').limit(4)
   end
 
   def about
