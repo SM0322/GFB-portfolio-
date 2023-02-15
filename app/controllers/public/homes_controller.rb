1a9129e03b2871e1c00106c603216f3bd689b62c
SM0322 <kaizer956@gmail.com>
2023-02-15 04:26:45 +0000

    Change top about layouts

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @post_favorite_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+    @post_favorite_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(4).pluck(:post_id))
     @post_news = Post.order('id DESC').limit(4)
   end
 
