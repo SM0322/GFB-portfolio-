36cfe777517aa4a5d050508fb49dabdae478c8d7
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-02-19 06:32:07 +0000

    Add multiple preview function

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,47 +1,24 @@
-if (document.URL.match(/new/)){
+if (document.URL.match(/new/) || document.URL.match(/edit/)){
   document.addEventListener('DOMContentLoaded', () => {
-    const createImageHTML = (blob) => {
+    const createImageHTML = (blob) => {                                          
       const imageElement = document.getElementById('new-image');
       const blobImage = document.createElement('img');
-      blobImage.setAttribute('class', 'new-img')
+      blobImage.setAttribute('class', 'new-img');
       blobImage.setAttribute('src', blob);
       
       imageElement.appendChild(blobImage);
     };
     document.getElementById('post_images').addEventListener('change', (e) =>{
-
-      const imageContent = document.querySelector('img'); 
-      if (imageContent){
-        imageContent.remove();
-      } 
+      const imageContents = $(".new-img");
+      $.each(imageContents, function(idx, val) {
+        val.remove();
+      });
       
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-      createImageHTML(blob);
-    });
-  });
-}
-
-if (document.URL.match(/edit/)){
-  document.addEventListener('DOMContentLoaded', () => {
-    const createImageHTML = (blob) => {
-      const imageElement = document.getElementById('edit-image');
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('class', 'edit-img')
-      blobImage.setAttribute('src', blob);
-      
-      imageElement.appendChild(blobImage);
-    };
-    document.getElementById('post_images').addEventListener('change', (e) =>{
-
-      const imageContent = document.querySelector('img'); 
-      if (imageContent){
-        imageContent.remove();
-      } 
-      
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-      createImageHTML(blob);
+      const files = e.target.files;
+      for (let i = 0; i < files.length; i++) {
+        const blob = window.URL.createObjectURL(files[i]);
+        createImageHTML(blob);
+      };
     });
   });
 }
