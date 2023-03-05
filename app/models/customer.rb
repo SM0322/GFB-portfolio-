bead1ded64dd016e08deb0db60ef8e5bfe49f249
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-05 23:53:49 +0000

    migrateとmodelへの記述完了

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,12 +7,14 @@ class Customer < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
-  has_one_attached :profile_image
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :group_customers, dependent: :destroy
+  has_many :groups, through: :group_customers
+  
+  has_one_attached :profile_image
   
   validates :name, length: { in: 1..20 },
                    uniqueness: true
