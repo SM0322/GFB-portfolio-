1f8fa69ed7b48bf57b40e1edcdebc2be163af937
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-07 00:13:08 +0000

    モデルのアソシエーション完了

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   has_many :group_customers, dependent: :destroy
   has_many :customers, through: :group_customers
+  has_many :group_chats, dependent: :destroy
   
   has_one_attached :group_image
   
