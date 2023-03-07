6b94cb6009bc5c1979f7ac61ef5bcf8082fe1cea
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-07 00:20:58 +0000

    guestlogin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_05_233642) do
+ActiveRecord::Schema.define(version: 2023_03_07_000654) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -70,6 +70,14 @@ ActiveRecord::Schema.define(version: 2023_03_05_233642) do
   create_table "favorites", force: :cascade do |t|
     t.integer "customer_id"
     t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "group_chats", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "group_id"
+    t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
