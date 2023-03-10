2f542c75bdd59fe76684823d2339affac1b2fc4f
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-10 00:31:00 +0000

    Initial Commit

--- app/controllers/public/groups_controller.rb
+++
@@ -1,2 +0,0 @@
-class Public::GroupsController < ApplicationController
-end
--- app/models/group.rb
+++
@@ -1,9 +0,0 @@
-class Group < ApplicationRecord
-  has_many :group_customers, dependent: :destroy
-  has_many :customers, through: :group_customers
-  
-  has_one_attached :image
-  
-  validates :name, presence: true
-  validates :introduction, presence: true
-end
